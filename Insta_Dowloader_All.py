961b0801a1316ad8c6741f3da42d7b25e7a8fb44
balfarot <89477720+balfarot@users.noreply.github.com>
2022-04-30 14:56:20 -0700

    Add files via upload

---
+++ Insta_Dowloader_All.py
@@ -0,0 +1,9 @@
+# Download all the pictures of a instagram profile
+
+import instaloader
+
+from instaloader import Profile, Post
+insta = instaloader.Instaloader()
+insta.login(user="bre_nda_at", passwd="$MochaIsMyLove1154")
+#you will need firefox mozilla explorer
+insta.download_profile(profile_name=bre_nda_at)
